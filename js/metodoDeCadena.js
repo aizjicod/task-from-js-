04b324d01a7df77c0cfd428476e310120726e854
aizjicod <98921366+aizjicod@users.noreply.github.com>
2022-04-14 22:11:34 -0400

    did osme practice with methods of strings

---
+++ js/metodoDeCadena.js
@@ -0,0 +1,28 @@
+let cadena= "metodo de          cadena";
+let cadena2= "metodo de cadena cadena cadena cadena";
+let resultado ;
+
+// search for something and returns de value of an array or a variable.
+
+
+// resultado = cadena.concat(" hola");
+// resultado= cadena.startsWith("m");
+// resultado= cadena.endsWith("eda");
+// resultado= cadena.includes("pero");
+// resultado= cadena.indexOf("cadena");
+// resultado= cadena2.lastIndexOf("cadena");
+// resultado= cadena.padStart(21, "pipi ");
+// resultado= cadena.padEnd(21, " pipi");
+// resultado= cadena.repeat(2);
+
+
+//with this methods we can change some values inside of the array or the string
+
+// resultado=cadena.split(" ");
+// resultado=cadena.substring(0,6);
+// resultado=cadena.toLowerCase();
+// resultado=cadena.toUpperCase();
+// resultado=cadena.trim(); //if you want to try this one you could try adding some space in the string
+
+
+document.write(resultado + "<br>")
